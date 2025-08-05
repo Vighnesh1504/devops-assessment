10cff2d7534b29992b875dde2895a0f6ae99ccc8
Vighnesh1504 <146662865+Vighnesh1504@users.noreply.github.com>
2025-08-05 21:59:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 # Define the route for the home page
 @app.route('/')
 def hello():
-    return '<h1>Hello, World! This is my DevOps assessment project.</h1>'
+    return '<h1>Hello, World! This is my DevOps assessment!</h1>'
 
 # Run the application
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
